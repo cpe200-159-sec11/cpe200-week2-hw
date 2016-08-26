b078aa444f0c68136e58962c5205f4fba9f6d18a
Lenovo <poramin_s@cmu.ac.th>
2016-08-26 15:31:31 +0700

    Doubly Linked List

--- src/cpe200/week2/LinkedListTest.java
+++ src/cpe200/week2/LinkedListTest.java
@@ -12,21 +12,24 @@ public class LinkedListTest {
         l.pushToHead('E');
         l.pushToHead('#');
         l.pushToHead('3');
+        l.pushToTail('O');
+        l.pushToTail('A');
+        l.pushToTail('T');
 
         System.out.println("List Empty?: "+ l.isEmpty());
         System.out.println("List Size: "+l.getSize());
-        System.out.print(" "); l.printForward();
-        System.out.print(" "); l.printBackward();
+        System.out.println(" "); l.printForward();
+        System.out.println(" "); l.printBackward();
 
         System.out.println("Found 'I' in list?: "+ l.search('I'));
         System.out.println("Pop head: "+l.popHead());
 
-        System.out.print(" "); l.printBackward();
+        System.out.println(" "); l.printBackward();
 
         System.out.println("Pop tail: "+l.popTail());
 
-        System.out.print(" "); l.printForward();
-        System.out.print(" "); l.printBackward();
+        System.out.println(" "); l.printForward();
+        System.out.println(" "); l.printBackward();
 
         System.out.println("Found '3' in list?: "+ l.search('3'));
         System.out.println("List Size: "+l.getSize());
