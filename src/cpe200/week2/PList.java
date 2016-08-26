954ce379624bb7d25444f02d5576abae535df058
Chaloempong Budkrajang <chaloempong67@gmail.com>
2016-08-26 15:56:12 +0700

    done!

--- src/cpe200/week2/PList.java
+++ src/cpe200/week2/PList.java
@@ -15,6 +15,13 @@ public class PList {
 
     public void pushToTail(char i) {
         // implement your code here
+        PNode tmp = new PNode(i, null, tail);
+        if(tail == null){
+            tail = head;
+        }
+        tail.next = tmp;
+        tail = tmp;
+        size++;
     }
 
     public char popHead() {
@@ -36,31 +43,69 @@ public class PList {
 
     public char popTail() {
         // implement your code here
+        char i = tail.data;
+        PNode tmp = tail; // createe tmp = last node
+        if (tail.prev == null) { // check empty
+            return 0;
+        }
+        else
+            tail = tmp.prev; // point tail to new last node
+        tail.next = null;  // set last node = null
+        size--;
+        return i;
+
     }
 
     public boolean search(char i) {
         // implement your code here
+        PNode temp = tail;
+        while (temp != null) {
+            if (temp.data == i) { // if data == input return ture
+                return true;
+            }
+            temp = temp.prev; // move list
+        }
+        return false;
     }
 
     public boolean isEmpty() { return (head == null); }
 
     public void printForward() {
-        PNode tmp = head;
-
-        while (tmp != null) {
-            // implement your code here
+        if (head == tail)
+        {
+            System.out.print(head.data);
         }
-        System.out.println();
+        else {
+            PNode tmp = head;
+            while (tmp != null) {
+                // implement your code here
+                System.out.print(tmp.data);
+                tmp = tmp.next;
+            }
+            System.out.println();
+        }
     }
 
     public void printBackward() {
         // implement your code here
+        if (tail == head)
+        {
+            System.out.print(tail.data);
+        }
+        else {
+            PNode tmp = tail;
+            while (tmp != null) {
+                // implement your code here
+                System.out.print(tmp.data);
+                tmp = tmp.prev;
+            }
+            System.out.println();
+        }
     }
 
     public int getSize() {
         return size;
     }
-
     private PNode head, tail;
     private int size=0;
 }
