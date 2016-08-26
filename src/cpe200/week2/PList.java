e3a45bc9da88458c113ac318e1e1f5a5217b9e50
isne339 <na_hahaha@hotmail.com>
2016-08-26 15:57:54 +0700

    Fill in the blank function with comments

--- src/cpe200/week2/PList.java
+++ src/cpe200/week2/PList.java
@@ -14,7 +14,14 @@ public class PList {
     }
 
     public void pushToTail(char i) {
-        // implement your code here
+        head = new PNode(i, null, tail);
+       if (head==null) {
+           tail = head;     //If nothing in list set head point to same data as tail
+       }
+        else {
+           tail.next.prev = tail; //new tail point prev to old tail
+       }
+       size++; //increase size
     }
 
     public char popHead() {
@@ -35,26 +42,56 @@ public class PList {
     }
 
     public char popTail() {
-        // implement your code here
+        char i=tail.data; //set char i contain tail data
+        PNode tmp = tail; //set tmp Node point at tail
+        if(head==tail) {
+            tail = head = null; //if have only one node set both head and tail point to null
+        }
+        else {
+            tail = tail.prev;
+            tail.next = null;   //move tail pointer point to prev tail
+            tmp.prev = null;    //delete tail
+        }
+        size--; //decrease size
+        return i; //return the value of deleted tail
     }
 
     public boolean search(char i) {
-        // implement your code here
+        PNode tmp = head; //set tmp point to data as head point to
+        if(head == null && tail == null)
+        {
+            return false;       //if nothing in the list
+        }
+        while(tmp != null)
+        {
+            if(tmp.data==i) {
+                return true;    //if data in tmp node equal to i return true
+            }
+            else {
+                tmp = tmp.next; //if not move to next node
+            }
+        }
+        return false; //if end of the loop but still didnt find return false
     }
 
     public boolean isEmpty() { return (head == null); }
 
     public void printForward() {
         PNode tmp = head;
-
         while (tmp != null) {
-            // implement your code here
+            System.out.print(tmp.data); //print data that tmp point to
+            tmp = tmp.next; //move tmp to next node
         }
         System.out.println();
     }
 
     public void printBackward() {
-        // implement your code here
+        PNode tmp = tail; //set tmp point to data that tail point to
+        while (tmp != null) {
+            System.out.print(tmp.data);  //print data that tmp point to
+            tmp = tmp.prev; //move tmp previous
+        }
+        System.out.println();
     }
 
     public int getSize() {
