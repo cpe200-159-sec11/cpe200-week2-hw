c8091362fa04e792995b809c1c16d609ea9b7d94
earnearn07 <dy]piy=9Ngh>
2016-08-27 19:35:23 +0700

    Finished

--- src/cpe200/week2/PList.java
+++ src/cpe200/week2/PList.java
@@ -15,6 +15,12 @@ public class PList {
 
     public void pushToTail(char i) {
         // implement your code here
+        tail = new PNode(i, null, tail);
+        if (head==null)
+            head = tail;
+        else
+            tail.next.prev = tail;
+        size++;
     }
 
     public char popHead() {
@@ -28,33 +34,58 @@ public class PList {
             head.prev = null;
             tmp.next = null;
         }
-
         size--;
-
         return i;
     }
 
     public char popTail() {
         // implement your code here
+        char i=tail.data;
+        PNode tmp = tail;
+
+        if (head==tail)
+            head = tail = null;
+        else {
+            tail = tail.prev;
+            tail.next = null;
+            tmp.next = null;
+        }
+        size--;
+        return i;
     }
 
     public boolean search(char i) {
         // implement your code here
+        PNode tmp;
+        for (tmp = head; tmp != null && (tmp.data!=i); tmp = tmp.next);
+        return tmp!=null;
     }
 
     public boolean isEmpty() { return (head == null); }
+        //if(head==null) return true; else return false;
+
 
     public void printForward() {
         PNode tmp = head;
 
         while (tmp != null) {
             // implement your code here
+            System.out.println(tmp.data);
+            tmp = tmp.next;
         }
         System.out.println();
     }
 
     public void printBackward() {
         // implement your code here
+        PNode tmp = tail;
+
+        while (tmp != null)
+        {
+            System.out.println(tmp.data);
+            tmp = tmp.prev;
+        }
+        System.out.println();
     }
 
     public int getSize() {
